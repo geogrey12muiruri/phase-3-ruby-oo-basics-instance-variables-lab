c5a38e1d4cf2cb74f44cfa07e8884519bceb99c3
geogrey12muiruri <greggambugua@gmail.com>
2022-10-23 21:41:26 +0300

    complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    def name=(dog_name)
+      @this_dogs_name = dog_name
+    end
+    def name
+      @this_dogs_name
+    end
+ end
 
-end
+
+#in the code above we define two seperate instances the first instance is #name= the second instance is #name
+#the first method takes one argument that is the dog_name and assaign it to a variable this_dogs_name
+#the second name is for reporting the name.
+
+# THIS METHOD CAN BE VIEWED AS A GATEWAY OR A WAY WE TRANSFER INFORMATION FROM THE INSIDE OF THE OBJECT TO THE OUTSIDE OF THE WORLD.
+
+#our two methods are responsible for getting and setting an individual dog's name
