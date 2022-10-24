e806a7442752983ca0dae277b693675d4f0eb263
geogrey12muiruri <greggambugua@gmail.com>
2022-10-24 04:30:49 +0300

    commit tree fb9d72d860bc897822a320f180dbe06aef58725b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Dog
     def name=(dog_name)
       @this_dogs_name = dog_name
@@ -8,10 +9,13 @@ class Dog
  end
 
 
-#in the code above we define two seperate instances the first instance is #name= the second instance is #name
-#the first method takes one argument that is the dog_name and assaign it to a variable this_dogs_name
-#the second name is for reporting the name.
+#we want the above mechanism to be a means through which we pass information from the object to the otside world.
 
-# THIS METHOD CAN BE VIEWED AS A GATEWAY OR A WAY WE TRANSFER INFORMATION FROM THE INSIDE OF THE OBJECT TO THE OUTSIDE OF THE WORLD.
+#creates a new instance of the Dog class
+lassie = Dog.new
 
-#our two methods are responsible for getting and setting an individual dog's name
+#calling the name method on our new instance
+ lassie.name = "Lassie"
+
+ lassie.name
+ 
